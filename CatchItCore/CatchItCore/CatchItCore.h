1338103e9ac5de74599aab4dd3f39b8cd7e78af5
hadiz <hadi.zamani@outlook.com>
2020-05-15 10:42:30 +0200

    Initial Commit

---
+++ CatchItCore/CatchItCore/CatchItCore.h
@@ -0,0 +1,19 @@
+//
+//  CatchItCore.h
+//  CatchItCore
+//
+//  Created by Hadi Zamani on 5/15/20.
+//  Copyright © 2020 SomeSimpleSolutions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CatchItCore.
+FOUNDATION_EXPORT double CatchItCoreVersionNumber;
+
+//! Project version string for CatchItCore.
+FOUNDATION_EXPORT const unsigned char CatchItCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CatchItCore/PublicHeader.h>
+
+
